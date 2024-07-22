abe5faee531298558d4fa41a2f5ba5730c22f205
Nathaniel Escaro <butterman423@gmail.com>
2024-07-22 19:31:38 -0400

    Initial template

---
+++ src/pages/Scaffold.tsx
@@ -0,0 +1,26 @@
+export default function Scaffold() {
+    return (
+        <>
+            <h1>Welcome</h1>
+            <div>
+                <p>I'm Nathaniel Escaro</p>
+                <p>
+                    An aspiring software developer passionate about 
+                    developing applications for use and enjoyment.
+                </p>
+                <a href="/">More About Me</a>
+            </div>
+            <h1>Experiences</h1>
+            <div>
+                { /* Sidebar displaying project names. Keychain wheel */ }
+                <div>Hello</div>
+
+                { /* Content / Project Description */ }
+                <div>World</div>
+            </div>
+
+            { /* Links footer */ }
+            <div></div>
+        </>
+    )
+}
