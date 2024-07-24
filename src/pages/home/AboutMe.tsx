3a1a6737dca23685b02c71a34dcf066c5ff2e4ff
Nathaniel Escaro <butterman423@gmail.com>
2024-07-23 23:36:13 -0400

    Added Resume

--- src/pages/home/AboutMe.tsx
+++ src/pages/home/AboutMe.tsx
@@ -4,7 +4,7 @@ export default function AboutMe() {
             <h1 className="text-center">Nathaniel Escaro</h1>
             <div className="row">
                 <div className="col">
-                    <a href="">Resume</a>
+                    <a href="/20240719_Resume.docx.pdf">Resume</a>
                     <a href="https://www.linkedin.com/in/nathaniel-andre-escaro-745099251">Linkdin</a>
                     <a href="https://github.com/butterman0423">Github</a>
                     <a href="https://butterman423.itch.io/">Itch.io</a>
