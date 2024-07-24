bd0b6c5c457245ff040c677fb9a4b42590a25c0e
Nathaniel Escaro <butterman423@gmail.com>
2024-07-23 23:14:14 -0400

    Simplified At a Glance

--- src/pages/home/GlanceItem.tsx
+++ src/pages/home/GlanceItem.tsx
@@ -1,17 +1,18 @@
 type Options = {
     title: string,
     quip: string,
+    link: string,
     onSeeMore?: () => void
 }
 
 export default function GlanceItem({
-    title, quip, onSeeMore
+    title, quip, link, onSeeMore
 }: Options) {
     return (
         <div className="col">
             <h2>{ title }</h2>
             <p>{ quip }</p>
-            <a href="#work" className="btn btn-primary" onClick={ onSeeMore }>See More</a>
+            <a href={link} className="btn btn-primary" onClick={ onSeeMore }>See More</a>
         </div>
     );
 }
--- src/pages/home/Main.tsx
+++ src/pages/home/Main.tsx
@@ -13,13 +13,16 @@ export default function Home() {
                 <div className="row">
                     <GlanceItem 
                         title="Inventory Buy/Sell Web App"
-                        quip="Record, Edit, and Track your Transactions in a Neat Spreadsheet." />
+                        quip="Record, Edit, and Track your Transactions in a Neat Spreadsheet."
+                        link="https://github.com/butterman0423/catalog-webapp" />
                     <GlanceItem
                         title="Latex Viewer Obsidian Plugin"
-                        quip="View Rendered LaTeX as you type in Obsidian." />
+                        quip="View Rendered LaTeX as you type in Obsidian."
+                        link="https://github.com/butterman0423/latex-viewer" />
                     <GlanceItem
                         title="Game Projects"
-                        quip="A Catalog of all my Released Games developed in Godot." />
+                        quip="A Catalog of all my Released Games developed in Godot."
+                        link="https://butterman423.itch.io/" />
                 </div>
 
                 <h1 id="work">All My Work</h1>
