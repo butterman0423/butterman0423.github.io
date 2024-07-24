da81f21b90130b08c74a3f948723bc23ad2da8c5
Nathaniel Escaro <butterman423@gmail.com>
2024-07-24 00:17:55 -0400

    Formatted Link Buttons

--- src/pages/home/AboutMe.tsx
+++ src/pages/home/AboutMe.tsx
@@ -1,14 +1,23 @@
 export default function AboutMe() {
+    const links = [
+        { href: '/20240719_Resume.docx.pdf', text: 'Resume' },
+        { href: 'https://www.linkedin.com/in/nathaniel-andre-escaro-745099251', text: 'Linkdin' },
+        { href: 'https://github.com/butterman0423', text: 'Github' },
+        { href: 'https://butterman423.itch.io/', text: 'itch.io'}
+    ]
+    .map(({ href, text }) => {
+        return (
+            <div className="col">
+                <a className="btn btn-outline-secondary" href={href}>{ text }</a>
+            </div>
+        );
+    })
+
     return (
         <div className="container">
-            <h1 className="text-center">Nathaniel Escaro</h1>
-            <div className="row">
-                <div className="col">
-                    <a href="/20240719_Resume.docx.pdf">Resume</a>
-                    <a href="https://www.linkedin.com/in/nathaniel-andre-escaro-745099251">Linkdin</a>
-                    <a href="https://github.com/butterman0423">Github</a>
-                    <a href="https://butterman423.itch.io/">Itch.io</a>
-                </div>
+            <h1 className="text-center text-decoration-underline">Nathaniel Escaro</h1>
+            <div className="row row-cols-auto gx-2 justify-content-center">
+                { links }
             </div>
         </div>
     );
