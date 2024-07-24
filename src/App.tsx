d7cef19ebbe999d52d4104a39b01f18834b0e259
Nathaniel Escaro <butterman423@gmail.com>
2024-07-24 14:27:20 -0400

    Removed excess files/imports/styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
-//import Scaffold from './pages/Scaffold';
 import Home from './pages/home/Main';
-
-import './App.css'
 
 function App() {
   return (
