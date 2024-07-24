8784aa9414744e2e6a142ccbbe59fc07ced3ee29
Nathaniel Escaro <butterman423@gmail.com>
2024-07-24 18:29:43 -0400

    Added Link Highlighting to Project List

--- src/pages/home/Projects.tsx
+++ src/pages/home/Projects.tsx
@@ -5,6 +5,7 @@ type Project = {
     quip: string,
     icon: string,
     school?: boolean,
+    link?: string,
 }
 
 type Options = {
@@ -15,15 +16,19 @@ export default function Projects({
     projects
 }: Options) {
     const items = projects
-        .map(({ title, month, year, quip }) => {
+        .map(({ title, month, year, quip, link }) => {
             return (
                 <div className="col-4 text-center">
-                    <div className="bg-body-secondary border rounded h-100 p-4">
-                        <div className="h-100 d-flex flex-column align-items-center justify-content-center">
-                            <h3 className="mb-0 fw-bold">{ title }</h3>
-                            <p className="mb-2 fs-5">{ month } '{ year }</p>
-                            { quip.split('\n').map(v => <p className="m-0">{v}</p>) }
-                        </div>
+                    <div className="bg-body-secondary border rounded h-100">
+                        <a href={link} 
+                            className="btn btn-outline-primary border-0 w-100 h-100 p-4"
+                            style={{cursor: link ? "pointer" : "default"}}>
+                            <div className="h-100 clear-text d-flex flex-column align-items-center justify-content-center">
+                                <h3 className="mb-0 fw-bold">{ title }</h3>
+                                <p className="mb-2 fs-5">{ month } '{ year }</p>
+                                { quip.split('\n').map(v => <p className="m-0">{v}</p>) }
+                            </div>
+                        </a>
                     </div>
                 </div>
             );
